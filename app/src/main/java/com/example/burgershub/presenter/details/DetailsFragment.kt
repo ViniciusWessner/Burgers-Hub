a9a4500b1ec59942913e95fb0d079f5d37fff628
Vinicius <vini.wessner@gmail.com>
2024-08-22 14:50:25 -0300

    created view details burger

--- app/src/main/java/com/example/burgershub/presenter/details/DetailsFragment.kt
+++ app/src/main/java/com/example/burgershub/presenter/details/DetailsFragment.kt
@@ -5,9 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.burgershub.R
 import com.example.burgershub.databinding.FragmentDetailsBinding
+import com.example.burgershub.domain.model.Burger
+import com.example.burgershub.util.StateView
+import com.example.burgershub.util.formattedValue
+import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,6 +22,7 @@ class DetailsFragment : Fragment() {
 
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
+    private val viewModel:DetailsViewModel by viewModels()
 
     private val args: DetailsFragmentArgs by navArgs()
 
@@ -26,6 +34,45 @@ class DetailsFragment : Fragment() {
     ): View {
         _binding = FragmentDetailsBinding.inflate(inflater, container, false)
         return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        getBurgerById()
+        initListenears()
+    }
+
+    private fun initListenears(){
+        binding.btnBack.setOnClickListener{findNavController().popBackStack()}
+    }
+
+    private fun getBurgerById(){
+        viewModel.getBurgersById(args.burgerID).observe(viewLifecycleOwner){ stateView ->
+            when(stateView){
+                is StateView.Loading -> {
+
+                }
+                is StateView.Success -> {
+                    stateView.data?.let { configData(it) }
+                }
+                is StateView.Error -> {
+                    Toast.makeText(requireContext(), stateView.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun configData(burger: Burger) {
+        Picasso
+            .get()
+            .load(burger.image?.get(1)?.lg)
+            .into(binding.imageBurger)
+
+        binding.textBurger.text = burger.name
+        binding.textDescription.text = burger.desc
+        binding.textPrice.text = burger.price?.formattedValue()
 
 
     }
